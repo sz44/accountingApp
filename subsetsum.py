49691f27655a1c0b21d087176816a551ca86a2eb
sz44 <semion44@gmail.com>
2024-09-07 20:14:25 -0700

    added better var names and fixed for all pos ints

--- subsetsum.py
+++ subsetsum.py
@@ -11,23 +11,32 @@ def subsetSum(nums, k):
                 dp[row][col] = True
     return dp
 
-# condition 1: if nums == {} (empty set), False for all k
-# condition 2: if n == k, (where n is ith number of nums), True
-# condition 3: if row-1 == True (subset already True), True
-# condition 4: if k-n == True (in prev rows/ subset), True
+# condition a: if nums == {} (empty set), False for all k
+# condition b: if n == k, (where n is ith number of nums), True
+# condition c: if row-1 == True (subset already True), True
+# condition d: if k-n == True (in prev rows/ subset), True
 
 def subsetSum2(nums, k):
-    minVal = min(nums)
-    dp = [[False]*(abs(minVal)+k+1) for i in range(len(nums)+1)]
+    min_val = min(nums)
+    # for all positive integers
+    if min_val > 0:
+        min_val = 0
+    num_cols = abs(min_val)+k+1
+    dp = [[False]*num_cols for i in range(len(nums)+1)]
 
     for row in range(1, len(nums)+1):
-        for col in range(abs(minVal)+k+1):
-            # check condition 2,3,4
-            a = nums[row-1] == col - abs(minVal)
-            b = dp[row-1][col]
-            c =  0 <= col - nums[row-1] < abs(minVal)+k+1 and dp[row-1][col - nums[row-1]]
+        for col in range(num_cols):
+            # check condition b,c,d
+            current_sum = col - abs(min_val)
+            b = nums[row-1] == current_sum
 
-            dp[row][col] = a or b or c
+            c = dp[row-1][col]
+
+            new_col = col - nums[row-1]
+            is_valid_index = 0 <= col - nums[row-1] < num_cols
+            d = is_valid_index and dp[row-1][new_col]
+
+            dp[row][col] = b or c or d
 
     return dp[-1][-1]
 
